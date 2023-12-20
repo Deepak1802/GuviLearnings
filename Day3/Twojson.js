953fa4a81883ef0023b89e8002039da891a82abc
Deepak1802 <dpk.civil@gmail.com>
2023-12-20 21:37:42 +0530

    Update Twojson.js

--- Day3/Twojson.js
+++ Day3/Twojson.js
@@ -28,18 +28,9 @@ console.log(comparison);
 
 //question 2
 //use the rest countries api url and display all the country flag in console
-
-//1st step : create a XHR object
 var request = new XMLHttpRequest();
-//2nd step: API URL
-//.open method
-//2 parameters
 request.open("GET","https://restcountries.com/v2/all");
-//3rd step : establishing the bridge
 request.send();
-//4th step : once the data sucessfully received from server
-//onload is an event
-//when we use :  once the data / server is showing 200 sataus code
 request.onload=function(){
     var result=JSON.parse(request.response);
     for(let i = 0 ; i< result.length ; i++){
@@ -50,19 +41,9 @@ request.onload=function(){
 
 //question 3
 //use rest countries api url and print all countries names,regions,subregion and populations
-
-
-//1st step : create a XHR object
 var request = new XMLHttpRequest();
-//2nd step: API URL
-//.open method
-//2 parameters
 request.open("GET","https://restcountries.com/v2/all");
-//3rd step : establishing the bridge
 request.send();
-//4th step : once the data sucessfully received from server
-//onload is an event
-//when we use :  once the data / server is showing 200 sataus code
 request.onload=function(){
     var result=JSON.parse(request.response);
     for(let i = 0 ; i< result.length ; i++){
